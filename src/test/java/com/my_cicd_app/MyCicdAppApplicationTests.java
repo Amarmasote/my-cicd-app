a20f25a4c0154817bb923deb5b36df2f0378d032
Amar Masote <amarmasote@gmail.com>
2025-10-15 09:49:43 +0530

    Added sample test'

--- src/test/java/com/my_cicd_app/MyCicdAppApplicationTests.java
+++ src/test/java/com/my_cicd_app/MyCicdAppApplicationTests.java
@@ -2,12 +2,18 @@ package com.my_cicd_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class MyCicdAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void sampleTest() {
+		
 	}
 
 }
